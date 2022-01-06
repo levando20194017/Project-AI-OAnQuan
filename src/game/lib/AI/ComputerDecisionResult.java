82c3afc7cd410fcc66838c04a171109b17b47bd2
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-06 22:45:19 +0700

    add AI folder

---
+++ src/game/lib/AI/ComputerDecisionResult.java
@@ -0,0 +1,18 @@
+package game.lib.AI;
+
+import game.model.Direction;
+
+public class ComputerDecisionResult {
+    public int squareIndex;
+    public Direction dir;
+
+    public ComputerDecisionResult(int squareIndex, Direction dir) {
+        this.squareIndex = squareIndex;
+        this.dir = dir;
+    }
+
+    @Override
+    public String toString() {
+        return "should move at: " + squareIndex + " , dir: " + dir;
+    }
+}
