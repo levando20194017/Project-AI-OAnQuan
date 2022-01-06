f55da483100ffd4dbe4af3968bdad22d767005a1
levando20194017 <levando194017@gmail.com>
2022-01-06 22:58:56 +0700

    upload img into imgPool from asset

---
+++ src/game/loader/ImageLoader.java
@@ -0,0 +1,29 @@
+package game.loader;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+    Map<String, Image> imgPool = new HashMap<>();
+    {
+        File assertFile = new File("asset");
+        System.out.println(assertFile.getAbsolutePath());
+        for (File f : assertFile.listFiles()) {
+            try {
+                Image img = ImageIO.read(f);
+                imgPool.put(f.getName(), img);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Image getImg(String name) {
+        return imgPool.get(name);
+    }
+}
