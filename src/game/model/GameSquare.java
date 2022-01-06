7cf33c77f0e9acb62e89cee53cefb7f6bd136cb1
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-06 22:32:29 +0700

    updated gamesquare.js

---
+++ src/game/model/GameSquare.java
@@ -0,0 +1,54 @@
+package game.model;
+
+public class GameSquare {
+	private int squareIndex;
+	private Player player; // player 1, player 2
+	private int militaries;
+	private boolean isBossSquare;
+
+	// constructor
+	public GameSquare(int squareIndex, Player player, int militaries, boolean isBossSquare) {
+		this.squareIndex = squareIndex;
+		this.player = player;
+		this.militaries = militaries;
+		this.isBossSquare = isBossSquare;
+	}
+
+	public GameSquare(GameSquare gameSquare) {
+		this.squareIndex = gameSquare.squareIndex;
+		this.player = gameSquare.player.currentPlayer();
+		this.militaries = gameSquare.militaries;
+		this.isBossSquare = gameSquare.isBossSquare;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return squareIndex + " - " + militaries;
+	}
+
+	// setter, getter
+	public int getSquareIndex() {
+		return squareIndex;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public int getMilitaries() {
+		return militaries;
+	}
+
+	public void setMilitaries(int militaries) {
+		this.militaries = militaries;
+	}
+
+	public boolean isBossSquare() {
+		return isBossSquare;
+	}
+
+	public void setBossSquare(boolean v) {
+		this.isBossSquare = v;
+	}
+}
